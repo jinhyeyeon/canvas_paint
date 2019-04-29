e873ed02b6371ca712f961a6af57b0811eec921e
jinhyeyeon <jhy199@naver.com>
2019-04-29 22:14:00 +0900

    :sparkles: Footer Component
    #issue-7

---
+++ src/component/Footer/Footer.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import './footer.scss';
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <footer className="footer clearfix">
+                <p>
+                    그림판입니다 :)
+                </p>
+            </footer>
+        )
+    }
+}
+
+export default Footer;
