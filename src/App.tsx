ab510809aae2b38a46d15cbf0e03536c4ea6c469
jinhyeyeon <jhy199@naver.com>
2019-04-29 17:53:19 +0900

    :pencil: inedex.tsx 에서 import 수정
    #issue-5

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import * as React from 'react';
 import './styles/app.scss';
+import Header from './component/Header/Header';
 
 class App extends React.Component {
     render() {
         return (
             <div className="app">
-                
+                <Header />
             </div>
         )
     }
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/common.scss';
 import App from './App';
