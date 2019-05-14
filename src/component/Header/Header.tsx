6ccd570a6df390ef2e9aa398b0498e714203dfbc
jinhyeyeon <jhy199@naver.com>
2019-05-14 17:05:21 +0900

    :hammer: Header에 onChange 추가
    #issue-11

--- src/component/Header/Header.tsx
+++ src/component/Header/Header.tsx
@@ -1,50 +1,19 @@
 import * as React from 'react';
 import './header.scss';
-import classNames from 'classnames';
 import Controller from '../Controller/Controller';
-
-const COLOR = [
-    'red',
-    'orange',
-    'yellow',
-    'green',
-    'blue',
-    'indigo',
-    'purple'
-]
+import ColorPicker from '../ColorPicker/ColorPicker';
 
 interface Props {
-}
-interface State {
-    color: string;
-    on?: boolean;
+    onChange: (color: string) => void;
 }
 
-class Header extends React.Component<Props, State> {
-    state: State = {
-        color: COLOR[0],
-        on: false
-    }
+class Header extends React.Component<Props> {
     render() {
-        const {
-            
-        } = this.props;
-        const {
-            color,
-            on
-        } = this.state;
+        const {onChange} = this.props;
         return (
             <header className="header clearfix">
-                <Controller />
-                <ul className="colors">
-                    {COLOR.map(color => (
-                        <li
-                            className={classNames({on: color === this.state.color})}
-                            onClick={() => this.setState({color})}
-                            style={{backgroundColor: color}}
-                        />
-                    ))}
-                </ul>
+                <Controller/>
+                <ColorPicker onChange={onChange} />
             </header>
         )
     }
