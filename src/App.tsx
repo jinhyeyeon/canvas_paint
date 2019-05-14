053fbc3db5ea868c9830a020de50bea5303ad7f7
jinhyeyeon <jhy199@naver.com>
2019-05-14 17:05:53 +0900

    :hammer: App에 color state 추가
    #issue-11

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,32 @@ import './styles/app.scss';
 import {SketchField, Tools} from 'react-sketch';
 import Header from './component/Header/Header';
 import Footer from './component/Footer/Footer';
+import ColorPicker, {COLOR} from './component/ColorPicker/ColorPicker';
 
-class App extends React.Component {
+interface State {
+    color: string;
+}
+
+class App extends React.Component<{}, State> {
+    state: State = {
+        color: COLOR[0]
+    }
+
     render() {
+        const {color} = this.state;
+
         return (
             <div className="app">
-                <Header />
+                <Header
+                    onChange={color => {
+                        this.setState({color});
+                    }}
+                />
                 <SketchField
                     className="canvas-wrap"
                     height="785px" 
                     tool={Tools.Pencil} 
-                    lineColor="red"
+                    lineColor={color}
                     lineWidth={10}
                 />
                 <Footer />
